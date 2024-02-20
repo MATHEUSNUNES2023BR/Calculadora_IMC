4b90b795f4b29390112be72a7c7f44481b1ec083
Matheus Nunes <devmatheusnunesdebarros@gmail.com>
2024-02-20 16:53:02 -0300

    Exercício Finalizado

--- src/Form/index.jsx
+++ src/Form/index.jsx
@@ -6,23 +6,26 @@ function Form() {
   let [peso, setPeso] = useState(0)
   let [altura, setAltura] = useState(0)
   let [resultado, setResultado] = useState(0)
-    return(
+  let [erro, setErro] = useState(false)
+  return(
       <>
         <form className={styles.form}>
           <h1>Calculadora de IMC</h1>
           <div className={styles.formContainer}>
             <h2 className={styles.formContainerH2}>Altura:</h2>
-            <input onChange={evt => setAltura(evt.target.value)}  className={styles.formContainerInput} type="number" placeholder='Exemplo: 1.70' />
+            <input  onFocus={ evt => (setControlador(false), setErro(false))}   onChange={evt => setAltura(evt.target.value)}  className={styles.formContainerInput} type="number" placeholder='Exemplo: 1.70' />
           </div>
           <div className={styles.formContainer}>
             <h2 className={styles.formContainerH2}>Peso:</h2>
-            <input onChange={evt => setPeso(evt.target.value)} className={styles.formContainerInput} type="number" placeholder='Exemplo: 90.2' />
+            <input onFocus={ evt => (setControlador(false), setErro(false))} onChange={evt => setPeso(evt.target.value)} className={styles.formContainerInput} type="number" placeholder='Exemplo: 90.2' />
           </div>
-          <button className={styles.formButton}  onClick={evt =>
-             (setResultado((Number(peso) / (Number(altura) ** 2)).toFixed(2)), evt.preventDefault(), setControlador(true))}>Calcular
+          <div className={styles.formErro}>{erro&&<p className='erro'> VALOR INVÁLIDO!</p>} </div>
+          <button className={styles.formButton} onClick={evt =>
+             (setResultado((Number(peso) / (Number(altura) ** 2)).toFixed(2)), evt.preventDefault(), setControlador(true),
+              setErro((Number(peso) / (Number(altura) ** 2)) <= 0 || (Number(peso) / (Number(altura) ** 2)).toString() == 'NaN' || (Number(peso) / (Number(altura) ** 2)).toString() == 'Infinity'))}>Calcular
           </button>
         </form>
-        {controlador&&<Resultado valor={resultado}/>}
+        {!erro&&controlador&&<Resultado valor={resultado}/>}
       </>
     )
 }
--- src/Resultado/index.jsx
+++ src/Resultado/index.jsx
@@ -1,9 +1,5 @@
 import styles from './Resultado.module.css'
 function Resultado(props){
-    let valor = props.valor
-    if(valor <= 16.9){
-        var vv = styles.a1
-    }
     return(
         <>
             <div className={styles.resultado}>
@@ -17,31 +13,31 @@ function Resultado(props){
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
+                        <tr className={props.valor < 17 ? styles.cor : ''}>
                             <td className={styles.c1}>Menor que 16,9</td>
                             <td className={styles.c2}>Muito Abaixo do Peso</td>
                         </tr>
-                        <tr>
+                        <tr className={props.valor >= 17 && props.valor <= 18.4 ? styles.cor : ''}>
                             <td className={styles.c1}>Entre 17 e 18,4</td>
                             <td className={styles.c2}>Abaixo do Peso</td>
                         </tr>
-                        <tr>
+                        <tr className={props.valor >= 18.5 && props.valor <= 24.9 ? styles.cor : ''}>
                             <td className={styles.c1}>Entre 18,5 e 24,9</td>
                             <td className={styles.c2}>Peso Normal</td>
                         </tr>
-                        <tr>
+                        <tr className={props.valor >= 25 && props.valor <= 29.9 ? styles.cor : ''}>
                             <td className={styles.c1}>Entre 25 e 29,9</td>
                             <td className={styles.c2}>Acima do Peso</td>
                         </tr>
-                        <tr>
+                        <tr className={props.valor >= 30 && props.valor <= 34.9 ? styles.cor : ''}>
                             <td className={styles.c1}>Entre 30 e 34,9</td>
                             <td className={styles.c2}>Obesidade Grau I</td>
                         </tr>
-                        <tr>
+                        <tr className={props.valor >= 35 && props.valor <= 39.9 ? styles.cor : ''}>
                             <td className={styles.c1}>Entre 35 e 39,9</td>
                             <td className={styles.c2}>Obesidade Grau II</td>
                         </tr>
-                        <tr>
+                        <tr className={props.valor > 39.9 ? styles.cor : ''}>
                             <td className={styles.c1}>Acima de 40</td>
                             <td className={styles.c2}>Obesidade Grau III</td>
                         </tr>
